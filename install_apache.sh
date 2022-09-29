8671f6ceb7430ae1c613a4a90fb4ad63dad91fad
Alan <qiurihuanghua@126.com>
2022-09-29 14:41:17 +0300

    Revert "Add the initial version of codes to build a three-tier architecture"

--- install_apache.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-yum update -y
-yum install -y httpd.x86_64
-systemctl start httpd.service
-systemctl enbale httpd.service
-echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
